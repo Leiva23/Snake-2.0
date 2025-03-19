fe7938c88feeae5da42c3b9f2f01fe46dcdaad9f
Alex Leiva <alex.leiva@enti.cat>
2025-03-19 12:09:19 +0100

    Update main.cpp

--- Snake/main.cpp
+++ Snake/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <thread>
 #include <chrono>
+#include <time.h>
 #include "const.h"
 #include "keyboard.h"
 using namespace std;
@@ -55,77 +56,108 @@ void printTablero(char tablero[FILAS][COLUMNAS]) {
 
 void movimiento(position &cabezaSerpiente, char tablero[FILAS][COLUMNAS], bool &pressW, bool &pressA, bool &pressS, bool &pressD) {
 
-        if (IsWPressed()) {
+        if (IsWPressed() && !pressS) {
             pressW = true;
             pressA = false;
             pressS = false;
             pressD = false;
         }
 
-        if (IsAPressed()) {
+        if (IsAPressed() && !pressD) {
             pressW = false;
             pressA = true;
             pressS = false;
             pressD = false;
         }
-        if (IsSPressed()) {
+        if (IsSPressed() && !pressW) {
             pressW = false;
             pressA = false;
             pressS = true;
             pressD = false;
         }
-        if (IsDPressed()) {
+        if (IsDPressed() && !pressA) {
             pressW = false;
             pressA = false;
             pressS = false;
             pressD = true;
         }
 
-            if (pressW){
-                cabezaSerpiente.x--;
-            }
-            if (pressA){
-                cabezaSerpiente.y--;
-            }
-            if (pressS){
-                cabezaSerpiente.x++;
-            }
-            if (pressD){ 
-                cabezaSerpiente.y++;
-            }
+        if (pressW){
+            cabezaSerpiente.x--;
+        }
+        if (pressA){
+            cabezaSerpiente.y--;
+        }
+        if (pressS){
+            cabezaSerpiente.x++;
+        }
+        if (pressD){
+            cabezaSerpiente.y++;
+        }
     
-            for (int i = 0; i < FILAS; i++) {
-                for (int j = 0; j < COLUMNAS; j++) {
+        for (int i = 0; i < FILAS; i++) {
+            for (int j = 0; j < COLUMNAS; j++) {
+
+                if (tablero[i][j] == MANZANA) {
+                    cout << tablero[i][j];
+                }
+                else {
                     tablero[i][j] = ' ';
                 }
             }
-
-            tablero[cabezaSerpiente.x][cabezaSerpiente.y] = CABEZA_SERPIENTE;
+        }
     }
 
-bool margenes(bool &GameOver, position cabezaSerpiente, char tablero[FILAS][COLUMNAS]) {
+bool margenes( position cabezaSerpiente) {
+
+            if (cabezaSerpiente.x < 0) {
+                return true;
+            }
+            
+            else if (cabezaSerpiente.y < 0) { 
+                return true;
+            }    
+            
+            else if (cabezaSerpiente.x >= FILAS) {
+                return true;
+            }
+            else if (cabezaSerpiente.y >= COLUMNAS) { 
+                return true;
+            }
+        
+    
+        return false;
+}
+
+void generarManzana(position &manzanaRand, char tablero[FILAS][COLUMNAS]) { 
+    bool manzana = false;
     for (int i = 0; i < FILAS; i++) {
         for (int j = 0; j < COLUMNAS; j++) {
 
-            if (cabezaSerpiente.x == FILAS -1 || 0 ) {
-                GameOver = true;
+            if (tablero[i][j] == MANZANA) {
+                cout << tablero[i][j];
+                manzana = true;
             }
-            else if (cabezaSerpiente.y == COLUMNAS -1 || 0) { 
-                GameOver = true;
-            }    
         }
     }
-        return GameOver;
+            if (!manzana) {        
+                manzanaRand.x = rand() % 20;
+                manzanaRand.y = rand() % 10;
+
+                tablero[manzanaRand.x][manzanaRand.y] = MANZANA;
+            }
 }
 
 int main() {
+    srand(time(NULL));
+
     bool pressW = false;
     bool pressA = false;
     bool pressS = false;
     bool pressD = false;
     bool GameOver = false;
     position cabezaSerpiente;
-
+    position manzana;
     char tablero[FILAS][COLUMNAS];
     inicializarTablero(tablero, cabezaSerpiente);
 
@@ -135,8 +167,15 @@ int main() {
 
         system("cls");  
         printTablero(tablero);
+        generarManzana(manzana, tablero);
         movimiento(cabezaSerpiente, tablero, pressW, pressA, pressS, pressD);
+        
+        GameOver = margenes(cabezaSerpiente);
 
+        if (!GameOver) tablero[cabezaSerpiente.x][cabezaSerpiente.y] = CABEZA_SERPIENTE;
+       
+         
+    
         //Sleep main thread to control game speed execution
         std::this_thread::sleep_for(std::chrono::milliseconds(FRAME_RATE));
 
